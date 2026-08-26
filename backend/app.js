858fe76f75f491a6a9776ee6fa2603cad70378b1
Asiya Farhath <asiyacode@gmail.com>
2026-08-26 22:23:23 +0530

    fix: added render url

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,11 @@ const userRoutes = require("./routes/userRoutes");
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: [
+    "https://leaveportal.barabaricollective.org",
+    "https://leave-management-system-frontend-7r85.onrender.com",
+    "http://localhost:5173"
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
